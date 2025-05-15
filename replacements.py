32fe0d18738f84d856db64154cbbe48c0b6e8630
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2025-05-15 09:21:11 -0500

    update to 1.4.1
    Signed-off-by: Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>

--- replacements.py
+++ replacements.py
@@ -13,11 +13,11 @@ def variableReplace(app, docname, source):
 # dictionary structure.
 variable_replacements = {
     # This is used in install instructions, so should be a full version
-    "{InstallationVersion}" : "1.4.0",
+    "{InstallationVersion}" : "1.4.1",
     # This should be the same as the above except with any hyphen before
     # a release candidate replaced with dot, and an additional hyphen and
     # release number added
-    "{GitHubDownloadVersion}" : "1.4.0-1",
+    "{GitHubDownloadVersion}" : "1.4.1-1",
     "{userdocs}" : "https://apptainer.org/docs/user/main",
     "{adminversion}": "main",
     "{userversion}": "main",
